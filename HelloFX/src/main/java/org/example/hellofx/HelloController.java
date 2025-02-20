71d6ccfbdf1f52207921ee77c30ea6325d46b0d8
JacobWesthoff04 <mzufnz@gmail.com>
2025-02-20 15:53:04 -0600

    Video at 11:10

--- HelloFX/src/main/java/org/example/hellofx/HelloController.java
+++ HelloFX/src/main/java/org/example/hellofx/HelloController.java
@@ -1,10 +1,13 @@
 package org.example.hellofx;
 
+import javafx.beans.Observable;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebHistory;
 import javafx.scene.web.WebView;
 
 import java.net.URL;
@@ -19,15 +22,51 @@ public class HelloController implements Initializable {
     private TextField textField;
 
     private WebEngine engine;
+    private webHistory history;
+
+    private String homePage;
+
+    private double webZoom;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
         engine = webView.getEngine();
+        homePage = "www.google.com";
+        textField.setText(homePage);
+        webZoom = 1.0;
         loadPage();
     }
 
     public void loadPage() {
+        //engine.load("https://www.google.com");
+        engine.load("https://" + textField.getText());
+    }
 
+    public void refreshPage() {
+
+        engine.reload();
+    }
+
+    public void zoomIn() {
+
+        webZoom += 0.25;
+        webView.setZoom(1.25);
+    }
+
+    public void zoomOut() {
+
+        webZoom -= 0.25;
+        webView.setZoom(0.75);
+    }
+
+    public void displayHistory() {
+        history = engine.getHistory();
+        ObservableList<WebHistory.Entry> entries = history.getEntries();
+
+        for(WebHistory.Entry entry : entries) {
+
+            System.out.println(entry);
+        }
     }
 }
